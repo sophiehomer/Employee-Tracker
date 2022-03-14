4e0112f964a2961895820170e4e1f1c8f66c6e6a
sophiehomer <sophiehomer94@gmail.com>
2022-03-13 20:54:06 -0700

    fixed typo in connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,11 +2,9 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: 'localhost',
-  // Your MySQL username,
   user: 'root',
-  // Your MySQL password
   password: '',
-  database: 'election'
+  database: 'employee'
 });
 
 module.exports = db;
