21be722da941231dc05a88cdec314b0aba0ad131
sophiehomer <sophiehomer94@gmail.com>
2022-03-13 20:53:20 -0700

    installed dependencies and created mySQL connection and schema

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE department (
+ id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+ name VARCHAR(30)
+)
+
+CREATE TABLE role (
+ id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+ title VARCHAR(30), 
+ salary decimal(10,2),
+ department_id INT
+)
+
+CREATE TABLE employee (
+ id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+ first_name VARCHAR(30),
+ last_name VARCHAR(30),
+ role_id INT,
+ manager_id INT
+)
