a4ee446f835046cb28844f4bf47f73f7441fb28b
sophiehomer <sophiehomer94@gmail.com>
2022-03-14 21:15:48 -0700

    updated seeds.sql

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+
 //need to check what my route is
+//&&
+//intalled dependencies npm i inquirer, npm i mysql2, npm i console.table, npm i
+
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'employee'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "employee"
 });
 
 module.exports = db;
--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE role (
  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
  title VARCHAR(30), 
  salary decimal(10,2),
- department_id INT
+ department_id INTEGER
 )
 
 /* -------------------------------- EMPLOYEE -------------------------------- */
@@ -17,6 +17,6 @@ CREATE TABLE employee (
  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
  first_name VARCHAR(30),
  last_name VARCHAR(30),
- role_id INT,
- manager_id INT
+ role_id INTEGER,
+ manager_id INTEGER
 )
